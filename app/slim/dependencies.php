96cda7e07e0db99096721230e002171a4b1f7538
Ansas <mail@dhm80.de>
2016-09-02 20:31:21 +0200

    Updated dependencies

--- app/slim/dependencies.php
+++ app/slim/dependencies.php
@@ -1,14 +1,18 @@
 <?php
 
 // Register service providers & factories
+
+// Default providers
 $container->register(new Ansas\Slim\Provider\DataProvider());
 $container->register(new Ansas\Slim\Provider\ExtendedRequestProvider());
 $container->register(new Ansas\Slim\Provider\ExtendedRouterProvider());
 $container->register(new Ansas\Slim\Provider\ErrorProvider());
-$container->register(new Ansas\Slim\Provider\FlashProvider());
-$container->register(new Ansas\Slim\Provider\LoggerProvider());
 $container->register(new Ansas\Slim\Provider\NotAllowedProvider());
 $container->register(new Ansas\Slim\Provider\NotFoundProvider());
 $container->register(new Ansas\Slim\Provider\TwigProvider());
-$container->register(new Ansas\Slim\Provider\LocaleProvider());
-$container->register(new Ansas\Slim\Provider\PropelProvider());
+
+// Optional providers
+//$container->register(new Ansas\Slim\Provider\FlashProvider());
+//$container->register(new Ansas\Slim\Provider\LocaleProvider());
+//$container->register(new Ansas\Slim\Provider\LoggerProvider()); // needs "monolog/monolog" package
+//$container->register(new Ansas\Slim\Provider\PropelProvider()); // needs "propel/propel" package
