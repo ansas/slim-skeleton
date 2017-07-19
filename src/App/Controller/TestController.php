14c71381ee954c5e3cee1fd043424b66c1458bdc
Ansas <mail@dhm80.de>
2017-07-19 21:31:37 +0200

    Minor bugfixes

--- src/App/Controller/TestController.php
+++ src/App/Controller/TestController.php
@@ -50,30 +50,6 @@ class TestController extends AbstractController
     }
 
     /**
-     * Invoke controller.
-     *
-     * @param Request  $request  The most recent Request object
-     * @param Response $response The most recent Response object
-     * @param array    $args
-     *
-     * @return Response
-     * @throws Exception
-     */
-    public function __invoke(Request $request, Response $response, array $args)
-    {
-        $method   = $args['method'];
-        $settings = $this->settings['test'];
-
-        if ($settings['always'] || $request->getParam($settings['key']) == $settings['value']) {
-            if (method_exists($this, $method)) {
-                return $this->$method($request, $response);
-            }
-        }
-
-        return $this->notFound($request, $response);
-    }
-
-    /**
      * Simulate "error".
      *
      * Usage:
